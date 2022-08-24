a3a46e57695c42812ee18c885fd55a139b6fb2c0
Inna <shvartz_i@mail.ru>
2022-08-24 08:05:25 +0300

    Square changed

--- Figures/Square.java
+++ Figures/Square.java
@@ -6,11 +6,6 @@ public class Square extends Rectangle {
         
     public Square (int side){ 
         super(side);
-        this.side=side;
-    }
-    @Override
-    public double area() {
-        return this.side * this.side;
     }
 
 }
